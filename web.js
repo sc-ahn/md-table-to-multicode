079f982945a6d70bf35c1ac595b7403fe08cb504
안성철 <scahn@huray.net>
2024-02-16 14:43:03 +0900

    Fix: bug
    ref wrong var name

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 const copyToClipboard = async () => {
-  const text = document.getElementById('output-textarea').value;
+  const value = document.getElementById('output-textarea').value;
   if (!value) {
     // no value to copy
     return;
   }
   try{
-    await navigator.clipboard.writeText(text);
+    await navigator.clipboard.writeText(value);
     alert("Copied the text")
   } catch (error) {
     console.error("Error copying text: ", error);
