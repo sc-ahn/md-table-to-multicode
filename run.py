762b46ba1261661ddb64f732315cba526399c803
안성철 <scahn@huray.net>
2024-02-16 09:25:40 +0900

    Update: executable script w/ example

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
+import sys
+
 from convert import (
     read_markdown_as_table,
     convert_table_as_multiline_text,
 )
 
 if __name__ == "__main__":
-    table = read_markdown_as_table("TABLE.md")
-    with open("table.txt", "w", encoding="utf-8") as file:
+    # 실행시 파일경로를 인자로 받을 수 있음
+    # 1. 입력파일경로
+    # 2. 출력파일경로
+    # 둘 다 옵셔널하게 받을 수 있음
+
+    # 입력파일경로
+    try:
+        input_path = sys.argv[1]
+    except IndexError:
+        input_path = "INPUT.md"
+
+    # 출력파일경로
+    try:
+        output_path = sys.argv[2]
+    except IndexError:
+        output_path = "OUTPUT.txt"
+
+    table = read_markdown_as_table(input_path)
+    with open(output_path, "w", encoding="utf-8") as file:
         file.write(convert_table_as_multiline_text(table))
