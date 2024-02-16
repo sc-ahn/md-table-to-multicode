40e3bee65747ce21a8a6b07c85f2a8ef35232fef
안성철 <scahn@huray.net>
2024-02-16 10:17:10 +0900

    Fix: no output, no copy

--- web.js
+++ web.js
@@ -1,5 +1,9 @@
 const copyToClipboard = async () => {
   const text = document.getElementById('output-textarea').value;
+  if (!value) {
+    // no value to copy
+    return;
+  }
   try{
     await navigator.clipboard.writeText(text);
     alert("Copied the text")
